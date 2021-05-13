2445b56792d3b5cd049df5b73769ee426ae015ac
T U <tylerletendre98@gmail.com>
2021-05-12 20:53:51 -0400

    major made a post request for both a comment and a reply

--- ctBackEnd/routes/ytclone.js
+++ ctBackEnd/routes/ytclone.js
@@ -13,5 +13,35 @@ router.get('/comments', async (req,res)=>{
     }
 });
 
+//POST A COMMENT
+
+router.post('/comment', async (req,res)=>{
+        try{
+            const comment = new Comment({
+            text: req.body.params
+            });
+    
+            await comment.save();
+            return res.send(comment);
+        }catch(ex){
+            return res.status(500).send(`Internal Server Error: ${ex}`);
+        }
+});
+
+//POST A REPLY
+router.post('/:commentId', async (req,res)=>{
+    try{
+        const comment = await Comment.findById(req.params.commentId);
+    
+        const reply = new Reply({
+            text: req.params.body
+        });
+        await comment.save();
+        const comments = await Comment.find();
+        return res.send(comments);
+    }catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
 
 module.exports = router;
