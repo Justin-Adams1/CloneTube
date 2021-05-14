4396c24055b36f189649b37538d34253ef6f4c45
tylerletendre98 <80071884+tylerletendre98@users.noreply.github.com>
2021-05-14 08:17:56 -0400

    Delete default.js

--- ctfrontend/config/default.js
+++
@@ -1,5 +0,0 @@
-const myKeys={
-    mongoURI: "mongodb+srv://vader:midnight55@cluster0.bmb1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    apiKEY: "AIzaSyDN2te2o9u00LvsFTyJ0Tn3p2mcKt8X1gc"
- }
- module.exports= myKeys;
