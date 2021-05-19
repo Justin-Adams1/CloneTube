ab65b18accdef573113119ee15efa6fb6109bae7
T U <tylerletendre98@gmail.com>
2021-05-18 20:54:11 -0400

    minor added in small things to the axios calls

--- ctfrontend/src/App.js
+++ ctfrontend/src/App.js
@@ -10,46 +10,51 @@ import Comments from './components/comments/comments';
 import {Key} from './key.js';
 import {Container, Row, Col} from 'react-bootstrap';
 
+
+
+
 function App(){
   
   // const apiKEY = config.apiKEY
   
-  // const [videoId, getVideoId] = useState('');
-  const videoId = "JuYeHPFR3f0"
+  const [videoId, getVideoId] = useState('');
+  // const videoId = "JuYeHPFR3f0"
   const [comment, getComment] = useState([]);
-  const [searchString, setSearchString] = useState('');
+  const [searchString, setSearchString] = useState('pokemon');
   const [relatedVideos, getRelatedVideos] = useState([]);
   const apiKEY = Key;
   
-  // useEffect(()=>{ // get search videoId
-  //   axios
-  //   .get(`https://www.googleapis.com/youtube/v3/search?q=${searchString}&key=${apiKEY}`)
-  //   .then(response => getVideoId(response.data.items[0].id.videoId))
-
-    
-  // },[searchString]);
-
-  useEffect(()=>{ //get related videos
+  useEffect(()=>{ // get search videoId
     axios
-    .get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${searchString}&key=${apiKEY}`)
-    .then(response => getRelatedVideos(response.data))
-    console.log(relatedVideos)
+    .get(`https://www.googleapis.com/youtube/v3/search?q=${searchString}&key=${apiKEY}`)
+    .then(response => getVideoId(response.data.items[0].id.videoId))
 
-  }, [searchString])
-
-  useEffect(()=>{ //get attached comment
+    axios
+    .get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=JuYeHPFR3f0&type=video&key=${apiKEY}`)
+    .then(response => getRelatedVideos(response))
+    
     axios
     .get(`http://localhost:5000/api/ytclone/${videoId}`)
     .then(response => getComment(response.data))
-    console.log(comment)
     
-  }, [comment]);
+  },[searchString]);  
   
   const addNewComment=(newComment)=>{
-     axios.post(`http://localhost:5000/api/ytclone/JuYeHPFR3f0`, newComment)
-     .then(response=>getComment(response.data))
+    axios.post(`http://localhost:5000/api/ytclone/JuYeHPFR3f0`, newComment)
+    .then(response=>getComment(response.data))
   }
-  
+  // useEffect(() => {
+  //   const getAllPeople = async () => {
+  //     const { data } = await axios.get("https://swapi.dev/api/people");
+  //     setPeople(data.results);
+  //     setIsLoaded(true);
+  //     console.log(data.results);
+  //     console.log(people);
+  //   };
+  //   getAllPeople();
+  // }, []);
+
+  console.log(relatedVideos)
   const videoRef = `https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com`
   
   return(
