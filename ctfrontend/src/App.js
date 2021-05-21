261560580c802a8f10991d0e2097c4c96190fb83
Justin Adams <khisanth05@gmail.com>
2021-05-20 20:17:53 -0400

    Minor - updated spacing

--- ctfrontend/src/App.js
+++ ctfrontend/src/App.js
@@ -8,38 +8,39 @@ import MainVideo from "./components/mainvideo/mainVideo";
 import Comments from "./components/comments/comments";
 import { Key } from "./key.js";
 import { Container, Row, Col } from "react-bootstrap";
-// import SearchResults from "./videoResults";
+import SearchResults from "./videoResults";
+import RelatedResults from "./relatedResults";
 
 function App() {
   // const apiKEY = config.apiKEY
-  // const searchResults = SearchResults;
-  const [videoId, setVideoId] = useState('');
-  // const videoId = "JuYeHPFR3f0"
+  const searchResults = SearchResults;
+  const relatedResults = RelatedResults;
+  const [videoId, setVideoId] = useState('JuYeHPFR3f0');
   const [comments, setComments] = useState([]);
   const [searchString, setSearchString] = useState("");
   const [relatedVideos, getRelatedVideos] = useState([]);
   const apiKEY = Key;
   const commentId = comments._id;
 
-  useEffect(() => {
-    axios
-      .get(`https://www.googleapis.com/youtube/v3/search?q=${searchString}&key=${apiKEY}`)
-      .then((response) => setVideoId(response.data.items[0].id.videoId));
-      console.log(videoId)
-  }, [searchString]);
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://www.googleapis.com/youtube/v3/search?q=${searchString}&key=${apiKEY}`)
+  //     .then((response) => setVideoId(response.data.items[0].id.videoId));
+  //     console.log(videoId)
+  // }, [searchString]);
 
-  useEffect(() => {
-    axios
-      .get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoId}type=video&part=snippet&key=${apiKEY}`)
-      .then((response) => getRelatedVideos(response));
-    console.log((relatedVideos));
-  }, [videoId]);
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoId}type=video&part=snippet&key=${apiKEY}`)
+  //     .then((response) => getRelatedVideos(response));
+  //   console.log((relatedVideos));
+  // }, [videoId]);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/ytclone/${videoId}`)
-      .then((response) => setComments(response.data));
-  }, [comments, videoId]);
+  // useEffect(() => {
+  //   axios
+  //     .get(`http://localhost:5000/api/ytclone/${videoId}`)
+  //     .then((response) => setComments(response.data));
+  // }, [comments, videoId]);
 
   // useEffect(()=>{
 
---
+++ ctfrontend/src/relatedResults.js
@@ -0,0 +1,228 @@
+const relatedResults = {
+  "kind": "youtube#searchListResponse",
+  "etag": "QONRnPJ9gMZh5wA3mtcPyRa_m8g",
+  "nextPageToken": "CAUQAA",
+  "regionCode": "US",
+  "pageInfo": {
+    "totalResults": 41,
+    "resultsPerPage": 5
+  },
+  "items": [
+    {
+      "kind": "youtube#searchResult",
+      "etag": "NoDZGJoSW5MlRaAB2p8tiBfwZ20",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "A_HjMIjzyMU"
+      },
+      "snippet": {
+        "publishedAt": "2019-05-08T15:43:29Z",
+        "channelId": "UC3gNmTGu-TTbFPpfSs5kNkg",
+        "title": "Shrek 2 (2004) - I Need a Hero Scene (7/10) | Movieclips",
+        "description": "Shrek 2 - I Need a Hero: Shrek (Michael Myers), Donkey (Eddie Murphy), Puss in Boots (Antonio Banderas), and the Gingerbread Men race to save Fiona (Cameron Diaz).\nBUY THE MOVIE: https://www.fandangonow.com/details/movie/shrek-2-2004/1MVa9f0a830468ec9a3f500a4d235ea0333?cmp=Movieclips_YT_Description\n\nWatch the best Shrek 2 scenes & clips:\nhttps://www.youtube.com/playlist?list=PLZbXA4lyCtqomJl4RLq4kmdT2SIhiD7U1\n\nFILM DESCRIPTION:\nAfter returning from their honeymoon and showing home movies to their friends, Shrek and Fiona learn that her parents have heard that she has married her true love and wish to invite him to their kingdom, called Far Far Away. The catch is: Fiona's parents are unaware of the curse that struck their daughter and have assumed she married Prince Charming, not a 700-pound ogre with horrible hygiene and a talking donkey pal.\n\nCREDITS:\nTM & © Dreamworks Animation (2004)\nCast: Antonio Banderas, Cameron Diaz, Eddie Murphy, Jennifer Saunders, Mike Myers, Rupert Everett\nDirector: Andrew Adamson, Conrad Vernon, Kelly Asbury\nScreenwriter: Andrew Adamson, David N. Weiss, J. David Stem, Joe Stillman\n\nWatch More:\n► Fresh New Clips: http://bit.ly/2taDWqW\n► Classic Trailers: http://bit.ly/2qTCxHF\n► Hot New Trailers: http://bit.ly/2qThrsF\n► Clips From Movies Coming Soon: http://bit.ly/2FrP8VL\n► Indie Movie Clips: http://bit.ly/2qTZMRE\n► Deleted Scenes: http://bit.ly/2ARbLPJ\n► Bloopers: http://bit.ly/2qYmBnc\n► Celebrity Interviews: http://bit.ly/2D4tzw4\n\nFuel Your Movie Obsession: \n► Subscribe to MOVIECLIPS: http://bit.ly/2CZa490\n► Watch Movieclips ORIGINALS: http://bit.ly/2D3sipV\n► Like us on FACEBOOK: http://bit.ly/2DikvkY\n► Follow us on TWITTER: http://bit.ly/2mgkaHb\n► Follow us on INSTAGRAM: http://bit.ly/2mg0VNU\n\nThe MOVIECLIPS channel is the largest collection of licensed movie clips on the web. Here you will find unforgettable moments, scenes, and lines from all your favorite films. Made by movie fans, for movie fans.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/A_HjMIjzyMU/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/A_HjMIjzyMU/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/A_HjMIjzyMU/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/A_HjMIjzyMU/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          },
+          "maxres": {
+            "url": "https://i.ytimg.com/vi/A_HjMIjzyMU/maxresdefault.jpg",
+            "width": 1280,
+            "height": 720
+          }
+        },
+        "channelTitle": "Movieclips",
+        "liveBroadcastContent": "none",
+        "publishTime": "2019-05-08T15:43:29Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "qMHUl3l7_nmFM5raC-YAADGQDqQ",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "xxttjs3Md2Q"
+      },
+      "snippet": {
+        "publishedAt": "2020-06-25T11:45:45Z",
+        "channelId": "UCFctpiB_Hnlk3ejWfHqSm6Q",
+        "title": "UK: Pokémon Journeys: Opening Theme (Full Song)",
+        "description": "Available to stream now: https://song.link/7dknqff7qnmtx\n\nAsh and Pikachu are almost ready for their new adventure, and with a new adventure comes a brand-new theme song! Written and performed by Walk off the Earth, “The Journey Starts Today” is available to stream right now.\n\nPokémon Journeys: The Series, the 23rd season of the iconic Pokémon animated series, will start airing on POP on 1 September: https://bit.ly/2xRMSrJ",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/xxttjs3Md2Q/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/xxttjs3Md2Q/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/xxttjs3Md2Q/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/xxttjs3Md2Q/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          },
+          "maxres": {
+            "url": "https://i.ytimg.com/vi/xxttjs3Md2Q/maxresdefault.jpg",
+            "width": 1280,
+            "height": 720
+          }
+        },
+        "channelTitle": "The Official Pokémon YouTube channel",
+        "liveBroadcastContent": "none",
+        "publishTime": "2020-06-25T11:45:45Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "VpLXVUgV7XK8dE2eaz6FJIlbbFI",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "LCNHc12volU"
+      },
+      "snippet": {
+        "publishedAt": "2018-04-25T23:43:24Z",
+        "channelId": "UCFctpiB_Hnlk3ejWfHqSm6Q",
+        "title": "Pokémon the Series Theme Songs—Kalos Region",
+        "description": "We’re kicking it back to Kalos today, Trainers! Ash and Pikachu explored this region with Serena, Clemont, and Bonnie by their side. \n\nDon’t forget, you can check out episodes on Pokémon TV! http://bit.ly/2HPAZ8w\n\nOfficial site: https://www.pokemon.com/ \nShop: http://www.pokemoncenter.com \nFacebook: http://www.facebook.com/Pokemon \nTwitter: http://www.twitter.com/Pokemon \nInstagram: http://www.instagram.com/pokemon \nTumblr: http://www.pokemon.tumblr.com",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/LCNHc12volU/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/LCNHc12volU/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/LCNHc12volU/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/LCNHc12volU/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          },
+          "maxres": {
+            "url": "https://i.ytimg.com/vi/LCNHc12volU/maxresdefault.jpg",
+            "width": 1280,
+            "height": 720
+          }
+        },
+        "channelTitle": "The Official Pokémon YouTube channel",
+        "liveBroadcastContent": "none",
+        "publishTime": "2018-04-25T23:43:24Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "fdV2SoraQcvC34Jvmn9erv_JWs8",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "TVcLIfSC4OE"
+      },
+      "snippet": {
+        "publishedAt": "2016-06-03T03:09:49Z",
+        "channelId": "UC_5niPa-d35gg88HaS7RrIw",
+        "title": "Mulan | I'll Make A Man Out Of You | Disney Sing-Along",
+        "description": "Streaming Now on Disney+ – Sign Up at https://disneyplus.com/\n\nBe swift as a coursing river as you sing along to \"I'll Make a Man Out of You\" from Mulan with this lyric video.\n\nSUBSCRIBE to get notified when new Disney videos are posted: http://di.sn/Subscribe\n\nGet even more Disney YouTube\nOh My Disney: https://www.youtube.com/user/OhMyDisney\nDisney Style: https://www.youtube.com/user/disneysstyle\nDisney Family: https://www.youtube.com/user/Disney\nMickey Mouse: https://www.youtube.com/user/DisneyShorts\n\nYou Might Also Like: \nAs Told By Emoji: https://www.youtube.com/playlist?list=PLpSnlSGciSWPZtUvlHLEp9_M5FPlC8LLa\nTsum Tsum Shorts: https://www.youtube.com/playlist?list=PLpSnlSGciSWPewHLHBq5HFLJBGhHrsKnJ\nSing Alongs: https://www.youtube.com/playlist?list=PLpSnlSGciSWPBUsHQKmXGX3TB-FTSO0U6\n\nMore Disney!\nInstagram: http://Instagram.com/Disney\nTwitter: http://Twitter.com/Disney\nFacebook: http://Facebook.com/Disney\nTumblr: http://disney.tumblr.com/\n\nA Little Disney History: From humble beginnings as a cartoon studio in the 1920s to its preeminent name in the entertainment industry today, Disney proudly continues its legacy of creating world-class stories and experiences for every member of the family.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/TVcLIfSC4OE/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/TVcLIfSC4OE/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/TVcLIfSC4OE/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/TVcLIfSC4OE/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          },
+          "maxres": {
+            "url": "https://i.ytimg.com/vi/TVcLIfSC4OE/maxresdefault.jpg",
+            "width": 1280,
+            "height": 720
+          }
+        },
+        "channelTitle": "Disney",
+        "liveBroadcastContent": "none",
+        "publishTime": "2016-06-03T03:09:49Z"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "AuGk7uAd7H6QOOXrhouQqnjkYAc",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "F_N5iaMHgKU"
+      },
+      "snippet": {
+        "publishedAt": "2019-07-08T18:13:50Z",
+        "channelId": "UCekLikuCSxA_gB7MW4trIlQ",
+        "title": "THE LION KING 'I Just Cant Wait To Be King' Official Clip (1994)| Disney Animation HD",
+        "description": "Watch the official clip compilation for The Lion King, an animation movie starring Matthew Broderick, Jonathan Taylor Thomas and James Earl Jones. Available on Digital HD and Blu-Ray now.\n\nA Lion cub crown prince is tricked by a treacherous uncle into thinking he caused his father's death and flees into exile in despair, only to learn in adulthood his identity and his responsibilities.\n\n© The Walt Disney Company. | All Rights Reserved.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/F_N5iaMHgKU/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/F_N5iaMHgKU/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/F_N5iaMHgKU/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/F_N5iaMHgKU/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          }
+        },
+        "channelTitle": "AniBox Trailer Access",
+        "liveBroadcastContent": "none",
+        "publishTime": "2019-07-08T18:13:50Z"
+      }
+    }
+  ]
+}
+export default relatedResults;
